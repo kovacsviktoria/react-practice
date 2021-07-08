d7f917d4f96fddc1ce41b80cf3df8d1242cb089b
Viktoria Toth <k.viki1117@gmail.com>
2021-07-08 11:31:32 +0200

    added click function

--- src/components/button/button.js
+++ src/components/button/button.js
@@ -1,8 +1,30 @@
 import './button.css';
 
+let timer;
+let prevHTML;
+/* window.hello = "helló";
+console.log(hello); */
+
+function clickHappened(e) {
+    //console.log(e.target);
+    
+    const goodHTML = "Goodbye";
+    
+    if (e.target.innerHTML !== goodHTML) {
+        prevHTML = e.target.innerHTML;
+        e.target.innerHTML = goodHTML;
+        timer = setTimeout(function(){ e.target.innerHTML=prevHTML }, 3000);
+    } else {
+        e.target.innerHTML = prevHTML;
+        clearTimeout(timer);
+        console.log(prevHTML);
+    }
+
+}
+
 function button(props) {
     return (
-        <button>{props.text}</button>
+        <button onClick={clickHappened}>{props.text}</button>
     );
 }
 
